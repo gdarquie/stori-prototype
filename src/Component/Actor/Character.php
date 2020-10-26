a6408e6813dc57ac4be9cf79289d67ecfba803c2
gdarquie <gaetan.darquie@gmail.com>
2020-10-26 23:54:43 +0100

    first commit

---
+++ src/Component/Actor/Character.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Component\Actor;
+
+class Character extends AbstractActor
+{
+    protected string $firstname;
+    protected string $lastname;
+}
